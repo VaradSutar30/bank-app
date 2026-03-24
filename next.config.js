8562c66c88cc9956c04edb66931a3768d47265ad
VaradSutar30 <varadsutar30@gmail.com>
2026-03-24 22:34:17 +0530

    Fix JSON + final config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { isServer }) => {
-    // ✅ Fix Tailwind fs errors for CLIENT bundle
     if (!isServer) {
       config.resolve.fallback = {
-        ...config.resolve.fallback,
         fs: false,
         path: false,
         module: false,
